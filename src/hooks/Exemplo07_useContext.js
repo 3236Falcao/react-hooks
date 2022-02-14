00fa532f3be1e98c3af138426f15a248e1888ff0
Daniel Paulino da Silva Falcao <daniel.falcao@havan.com.br>
2022-02-14 15:24:08 -0300

    manipulando elementos entre componentes com props

--- src/hooks/Exemplo07_useContext.js
+++ src/hooks/Exemplo07_useContext.js
@@ -1,8 +1,42 @@
+//manipulando elementos entre componentes com props
+
 import React from "react";
 
 function Principal(){
+
+    let texto = 'Enviando informações';
+
+    return(        
+        <Camada1 texto={texto}/>
+    );
+}
+
+//Camada 1
+function Camada1({texto}){
     return(
-        <h1>Componente Principal</h1>
+        <div style={{backgroundColor:'red', width:'500px', heigh: '500px', display:'inline-block'}}>         
+        <Camada2 texto={texto}/>
+        </div>
+    );
+}
+
+
+//Camada 2
+function Camada2({texto}){
+    return(
+        <div style={{backgroundColor:'green', width:'400px', margin: '50px', heigh: '400px',display:'inline-block' }}>
+        <Camada3 texto={texto}/>
+        </div>
+    );
+}
+
+
+//Camada 3
+function Camada3({texto}){
+    return(
+        <div style={{backgroundColor:'yellow', width:'300px', heigh: '300px', margin: '50px'}}>
+            <p>texto</p>
+        </div>
     );
 }
 
