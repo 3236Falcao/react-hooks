689b77143a05e6699256bca36acb9ff9f7cf3c4d
Daniel Paulino da Silva Falcao <daniel.falcao@havan.com.br>
2022-02-14 15:41:19 -0300

    manipulando elementos entre componentes com useContext

--- src/hooks/Exemplo07_useContext.js
+++ src/hooks/Exemplo07_useContext.js
@@ -1,41 +1,49 @@
-//manipulando elementos entre componentes com props
+//manipulando elementos entre componentes com useContext
 
-import React from "react";
+import React, {createContext, useContext} from "react";
+
+// criar contexto
+const Contexto =  createContext();
 
 function Principal(){
 
     let texto = 'Enviando informações';
 
-    return(        
-        <Camada1 texto={texto}/>
+    return( 
+        <Contexto.Provider value={{texto}}>       
+        <Camada1/>
+        </Contexto.Provider>
     );
 }
 
 //Camada 1
-function Camada1({texto}){
+function Camada1(){
     return(
         <div style={{backgroundColor:'red', width:'500px', heigh: '500px', display:'inline-block'}}>         
-        <Camada2 texto={texto}/>
+        <Camada2/>
         </div>
     );
 }
 
 
 //Camada 2
-function Camada2({texto}){
+function Camada2(){
     return(
         <div style={{backgroundColor:'green', width:'400px', margin: '50px', heigh: '400px',display:'inline-block' }}>
-        <Camada3 texto={texto}/>
+        <Camada3/>
         </div>
     );
 }
 
 
 //Camada 3
-function Camada3({texto}){
+function Camada3(){
+
+    const {texto} = useContext(Contexto);
+
     return(
         <div style={{backgroundColor:'yellow', width:'300px', heigh: '300px', margin: '50px'}}>
-            <p>texto</p>
+            <p>{texto}</p>
         </div>
     );
 }
