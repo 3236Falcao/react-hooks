3e01e29341c77eb2b5f1a6a65dd6520c412cda38
Daniel Paulino da Silva Falcao <daniel.falcao@havan.com.br>
2022-02-14 08:27:12 -0300

    criacao de componente principal com useContext

--- src/index.js
+++ src/index.js
@@ -8,11 +8,12 @@ import Exemplo03 from './hooks/Exemplo03_useState';
 import Exemplo04 from './hooks/Exemplo04_useState';
 import Exemplo05 from './hooks/Exemplo05_useEffect';
 import Exemplo06 from './hooks/Exemplo06_useEffect';
+import Exemplo07 from './hooks/Exemplo07_useContext';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Exemplo06 />
+    <Exemplo07 />
   </React.StrictMode>,
   document.getElementById('root')
 );
