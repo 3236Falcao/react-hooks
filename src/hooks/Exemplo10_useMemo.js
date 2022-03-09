816a8c8986437e36ffe4851d51d6268419cc52aa
Daniel Paulino da Silva Falcao <daniel.falcao@havan.com.br>
2022-03-09 15:00:39 -0300

    correcoes useMemo

--- src/hooks/Exemplo10_useMemo.js
+++ src/hooks/Exemplo10_useMemo.js
@@ -1,13 +1,13 @@
-import React, { useMemo, useState} from "react";
+import React, { useMemo, useState } from "react";
 
 function Exemplo10_useMemo() {
 
-const [contador, setContador] = useState(0);
+    const [contador, setContador] = useState(0);
 
     return (
         <div>
-            <h1>{contador}</h1> 
-            <button onClick={e => setContador(contador+1)}>Incrementar</button>       
+            <h1>{contador}</h1>
+            <button onClick={e => setContador(contador + 1)}>Incrementar</button>           
         </div>
     );
 }
